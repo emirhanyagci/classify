130c1336ff9419e9437dc7c357832dafce8d4383
Emirhan Yagci <emirhan.yacis@gmail.com>
2025-11-24 18:07:40 +0300

    Adds database module and migration

---
+++ apps/server/migrations/1759389124286_start-db.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.sql(`
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(100) NOT NULL,
+          email VARCHAR(150) UNIQUE NOT NULL,
+          created_at TIMESTAMP DEFAULT NOW()
+        );
+        INSERT INTO users (name, email) VALUES ('John Doe', 'john.doe@example.com');
+        INSERT INTO users (name, email) VALUES ('Jane Doe2', 'jane.doe2@example.com');
+      `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.sql(`
+        DROP TABLE users;
+    `)
+};
+
+
---
+++ apps/server/src/modules/db/db.module.ts
@@ -0,0 +1,25 @@
+import { Global, Module } from '@nestjs/common';
+import { Pool } from 'pg';
+@Global()
+@Module({
+    controllers: [],
+    providers: [
+        {
+            provide: "PG_POOL",
+            useFactory: () => {
+                return new Pool({
+                    host: 'localhost',
+                    port: 5432,
+                    database: 'app',
+                    user: 'app',
+                    password: 'app',
+                })
+            }
+        }
+    ],
+    exports: [
+        "PG_POOL",
+    ]
+
+})
+export class DbModule { }
